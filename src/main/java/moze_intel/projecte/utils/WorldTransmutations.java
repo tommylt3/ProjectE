4b0b348f1a9712862064b8c8701b3584297d7ddf
Thomas Gingerelli <tgingerelli3@gmail.com>
2023-04-14 23:01:07 -0400

    PE-12 DUDE

--- src/main/java/moze_intel/projecte/utils/WorldTransmutations.java
+++ src/main/java/moze_intel/projecte/utils/WorldTransmutations.java
@@ -23,19 +23,8 @@ public final class WorldTransmutations {
 	private static List<WorldTransmutationEntry> ENTRIES = Collections.emptyList();
 
 	public static void init() {
-		registerDefault(Blocks.STONE, Blocks.COBBLESTONE, Blocks.GRASS_BLOCK);
-		registerDefault(Blocks.COBBLESTONE, Blocks.STONE, Blocks.GRASS_BLOCK);
-		registerDefault(Blocks.GRASS_BLOCK, Blocks.SAND, Blocks.COBBLESTONE);
-		registerDefault(Blocks.DIRT, Blocks.SAND, Blocks.COBBLESTONE);
-		registerDefault(Blocks.SAND, Blocks.GRASS_BLOCK, Blocks.COBBLESTONE);
-		registerBackAndForth(Blocks.GRAVEL, Blocks.SANDSTONE);
 		registerBackAndForth(Blocks.WATER, Blocks.ICE);
 		registerBackAndForth(Blocks.LAVA, Blocks.OBSIDIAN);
-		registerBackAndForth(Blocks.MELON, Blocks.PUMPKIN);
-		registerDefault(Blocks.GRANITE, Blocks.DIORITE, Blocks.ANDESITE);
-		registerDefault(Blocks.DIORITE, Blocks.ANDESITE, Blocks.GRANITE);
-		registerDefault(Blocks.ANDESITE, Blocks.GRANITE, Blocks.DIORITE);
-
 		registerConsecutivePairsAllStates(Blocks.OAK_LOG, Blocks.BIRCH_LOG, Blocks.SPRUCE_LOG, Blocks.JUNGLE_LOG, Blocks.ACACIA_LOG, Blocks.DARK_OAK_LOG);
 		registerConsecutivePairsAllStates(Blocks.STRIPPED_OAK_LOG, Blocks.STRIPPED_BIRCH_LOG, Blocks.STRIPPED_SPRUCE_LOG, Blocks.STRIPPED_JUNGLE_LOG,
 				Blocks.STRIPPED_ACACIA_LOG, Blocks.STRIPPED_DARK_OAK_LOG);
@@ -79,10 +68,6 @@ public final class WorldTransmutations {
 		registerDefault(Blocks.NETHERRACK, Blocks.CRIMSON_NYLIUM, Blocks.WARPED_NYLIUM);
 		registerDefault(Blocks.CRIMSON_NYLIUM, Blocks.WARPED_NYLIUM, Blocks.NETHERRACK);
 		registerDefault(Blocks.WARPED_NYLIUM, Blocks.CRIMSON_NYLIUM, Blocks.NETHERRACK);
-      registerBackAndForth(Blocks.BROWN_MUSHROOM_BLOCK, Blocks.RED_MUSHROOM_BLOCK);
-      registerBackAndForth(Blocks.RED_MUSHROOM_BLOCK, Blocks.BROWN_MUSHROOM_BLOCK);
-      registerBackAndForth(Blocks.GLOWSTONE, Blocks.MAGMA_BLOCK);
-      registerBackAndForth(Blocks.MAGMA_BLOCK, Blocks.GLOWSTONE);
 		registerBackAndForthAllStates(Blocks.CRIMSON_STEM, Blocks.WARPED_STEM);
 		registerBackAndForthAllStates(Blocks.STRIPPED_CRIMSON_STEM, Blocks.STRIPPED_WARPED_STEM);
 		registerBackAndForth(Blocks.CRIMSON_HYPHAE, Blocks.WARPED_HYPHAE);
@@ -95,6 +80,126 @@ public final class WorldTransmutations {
 		registerBackAndForthAllStates(Blocks.CRIMSON_STAIRS, Blocks.WARPED_STAIRS);
 		registerBackAndForthAllStates(Blocks.CRIMSON_FENCE, Blocks.WARPED_FENCE);
 		registerBackAndForthAllStates(Blocks.CRIMSON_PRESSURE_PLATE, Blocks.WARPED_PRESSURE_PLATE);
+
+      // TODO Delete This Later
+      registerBackAndForthAllStates(Blocks.BROWN_MUSHROOM_BLOCK, Blocks.RED_MUSHROOM_BLOCK);
+      registerBackAndForthAllStates(Blocks.RED_MUSHROOM_BLOCK, Blocks.BROWN_MUSHROOM_BLOCK);
+      registerBackAndForthAllStates(Blocks.SPONGE, Blocks.WET_SPONGE);
+      registerConsecutivePairsAllStates(Blocks.GLOWSTONE, Blocks.MAGMA_BLOCK, Blocks.SHROOMLIGHT);
+      registerBackAndForthAllStates(Blocks.HONEY_BLOCK, Blocks.SLIME_BLOCK);registerBackAndForthAllStates(Blocks.RAW_IRON_BLOCK, Blocks.IRON_BLOCK);
+      registerBackAndForthAllStates(Blocks.PUMPKIN, Blocks.CARVED_PUMPKIN);
+      registerBackAndForthAllStates(Blocks.CARVED_PUMPKIN, Blocks.PUMPKIN);
+
+      // Light Family
+      registerBackAndForthAllStates(Blocks.LANTERN, Blocks.SOUL_LANTERN);
+      registerBackAndForthAllStates(Blocks.SOUL_LANTERN, Blocks.LANTERN);
+      registerBackAndForthAllStates(Blocks.CAMPFIRE, Blocks.SOUL_CAMPFIRE);
+      registerBackAndForthAllStates(Blocks.SOUL_CAMPFIRE, Blocks.CAMPFIRE);
+      registerBackAndForthAllStates(Blocks.TORCH, Blocks.SOUL_TORCH);
+      registerBackAndForthAllStates(Blocks.SOUL_TORCH, Blocks.TORCH);
+      registerBackAndForthAllStates(Blocks.FIRE, Blocks.SOUL_FIRE);
+      registerBackAndForthAllStates(Blocks.SOUL_FIRE, Blocks.FIRE);
+
+      // Ores Family
+      registerBackAndForthAllStates(Blocks.RAW_IRON_BLOCK, Blocks.IRON_BLOCK);
+      registerBackAndForthAllStates(Blocks.IRON_BLOCK, Blocks.RAW_IRON_BLOCK);
+      registerBackAndForthAllStates(Blocks.RAW_GOLD_BLOCK, Blocks.GOLD_BLOCK);
+      registerBackAndForthAllStates(Blocks.GOLD_BLOCK, Blocks.RAW_GOLD_BLOCK);
+      registerBackAndForthAllStates(Blocks.CRYING_OBSIDIAN, Blocks.OBSIDIAN);
+      registerBackAndForthAllStates(Blocks.OBSIDIAN, Blocks.CRYING_OBSIDIAN);
+
+
+      // Copper Family
+      registerDefault(Blocks.RAW_COPPER_BLOCK, Blocks.COPPER_BLOCK, Blocks.COPPER_BLOCK);
+      registerConsecutivePairsAllStates(Blocks.COPPER_BLOCK, Blocks.EXPOSED_COPPER, Blocks.WEATHERED_COPPER, Blocks.OXIDIZED_COPPER);
+      registerConsecutivePairsAllStates(Blocks.WAXED_COPPER_BLOCK, Blocks.WAXED_EXPOSED_COPPER, Blocks.WAXED_WEATHERED_COPPER, Blocks.WAXED_OXIDIZED_COPPER);
+      registerConsecutivePairsAllStates(Blocks.CUT_COPPER, Blocks.EXPOSED_CUT_COPPER, Blocks.WEATHERED_CUT_COPPER, Blocks.OXIDIZED_CUT_COPPER);
+      registerConsecutivePairsAllStates(Blocks.WAXED_CUT_COPPER, Blocks.WAXED_EXPOSED_CUT_COPPER, Blocks.WAXED_WEATHERED_CUT_COPPER, Blocks.WAXED_OXIDIZED_CUT_COPPER);
+      registerConsecutivePairsAllStates(Blocks.CUT_COPPER_SLAB, Blocks.EXPOSED_CUT_COPPER_SLAB, Blocks.WEATHERED_CUT_COPPER_SLAB, Blocks.OXIDIZED_CUT_COPPER_SLAB);
+      registerConsecutivePairsAllStates(Blocks.WAXED_CUT_COPPER_SLAB, Blocks.WAXED_EXPOSED_CUT_COPPER_SLAB, Blocks.WAXED_WEATHERED_CUT_COPPER_SLAB, Blocks.WAXED_OXIDIZED_CUT_COPPER_SLAB);
+      registerConsecutivePairsAllStates(Blocks.CUT_COPPER_STAIRS, Blocks.EXPOSED_CUT_COPPER_STAIRS, Blocks.WEATHERED_CUT_COPPER_STAIRS, Blocks.OXIDIZED_CUT_COPPER_STAIRS);
+      registerConsecutivePairsAllStates(Blocks.WAXED_CUT_COPPER_STAIRS, Blocks.WAXED_EXPOSED_CUT_COPPER_STAIRS, Blocks.WAXED_WEATHERED_CUT_COPPER_STAIRS, Blocks.WAXED_OXIDIZED_CUT_COPPER_STAIRS);
+
+     
+      // Stone Families
+      registerBackAndForthAllStates(Blocks.STONE, Blocks.SMOOTH_STONE);
+      registerBackAndForthAllStates(Blocks.SMOOTH_STONE, Blocks.STONE);
+      registerBackAndForthAllStates(Blocks.SMOOTH_STONE_SLAB, Blocks.STONE_SLAB);
+      registerBackAndForthAllStates(Blocks.STONE_SLAB, Blocks.SMOOTH_STONE_SLAB);
+      registerBackAndForthAllStates(Blocks.COBBLESTONE, Blocks.MOSSY_COBBLESTONE);
+      registerBackAndForthAllStates(Blocks.MOSSY_COBBLESTONE, Blocks.COBBLESTONE);
+      registerBackAndForthAllStates(Blocks.COBBLESTONE_SLAB, Blocks.MOSSY_COBBLESTONE_SLAB);
+      registerBackAndForthAllStates(Blocks.MOSSY_COBBLESTONE_SLAB, Blocks.COBBLESTONE_SLAB);
+      registerBackAndForthAllStates(Blocks.COBBLESTONE_STAIRS, Blocks.MOSSY_COBBLESTONE_STAIRS);
+      registerBackAndForthAllStates(Blocks.MOSSY_COBBLESTONE_STAIRS, Blocks.COBBLESTONE_STAIRS);
+      registerBackAndForthAllStates(Blocks.COBBLESTONE_WALL, Blocks.MOSSY_COBBLESTONE_WALL);
+      registerBackAndForthAllStates(Blocks.MOSSY_COBBLESTONE_WALL, Blocks.COBBLESTONE_WALL);
+      registerBackAndForthAllStates(Blocks.STONE_BRICKS, Blocks.MOSSY_STONE_BRICKS);
+      registerBackAndForthAllStates(Blocks.MOSSY_STONE_BRICKS, Blocks.STONE_BRICKS);
+      registerBackAndForthAllStates(Blocks.STONE_BRICK_SLAB, Blocks.MOSSY_STONE_BRICK_SLAB);
+      registerBackAndForthAllStates(Blocks.MOSSY_STONE_BRICK_SLAB, Blocks.STONE_BRICK_SLAB);
+      registerBackAndForthAllStates(Blocks.STONE_BRICK_STAIRS, Blocks.MOSSY_STONE_BRICK_STAIRS);
+      registerBackAndForthAllStates(Blocks.MOSSY_STONE_BRICK_STAIRS, Blocks.STONE_BRICK_STAIRS);
+      registerBackAndForthAllStates(Blocks.STONE_BRICK_WALL, Blocks.MOSSY_STONE_BRICK_WALL);
+      registerBackAndForthAllStates(Blocks.MOSSY_STONE_BRICK_WALL, Blocks.STONE_BRICK_WALL);
+
+
+      // Wish They Were Stone Families
+      registerConsecutivePairsAllStates(Blocks.DIORITE, Blocks.ANDESITE, Blocks.GRANITE, Blocks.GRAVEL);
+      registerConsecutivePairsAllStates(Blocks.DIORITE_SLAB, Blocks.ANDESITE_SLAB, Blocks.GRANITE_SLAB);
+      registerConsecutivePairsAllStates(Blocks.DIORITE_STAIRS, Blocks.ANDESITE_STAIRS, Blocks.GRANITE_STAIRS);
+      registerConsecutivePairsAllStates(Blocks.DIORITE_WALL, Blocks.ANDESITE_WALL, Blocks.GRANITE_WALL);
+      registerConsecutivePairsAllStates(Blocks.POLISHED_DIORITE, Blocks.POLISHED_ANDESITE, Blocks.POLISHED_GRANITE);
+      registerConsecutivePairsAllStates(Blocks.POLISHED_DIORITE_SLAB, Blocks.POLISHED_ANDESITE_SLAB, Blocks.POLISHED_GRANITE_SLAB);
+      registerConsecutivePairsAllStates(Blocks.POLISHED_DIORITE_STAIRS, Blocks.POLISHED_ANDESITE_STAIRS, Blocks.POLISHED_GRANITE_STAIRS);
+
+
+      // Sand + Sandstone
+      registerBackAndForthAllStates(Blocks.SAND, Blocks.RED_SAND);
+      registerBackAndForthAllStates(Blocks.RED_SAND, Blocks.SAND);
+      registerBackAndForthAllStates(Blocks.SANDSTONE, Blocks.RED_SANDSTONE);
+      registerBackAndForthAllStates(Blocks.RED_SANDSTONE, Blocks.SANDSTONE);
+      registerBackAndForthAllStates(Blocks.CHISELED_SANDSTONE, Blocks.CHISELED_RED_SANDSTONE);
+      registerBackAndForthAllStates(Blocks.CHISELED_RED_SANDSTONE, Blocks.CHISELED_SANDSTONE);
+      registerBackAndForthAllStates(Blocks.SMOOTH_SANDSTONE, Blocks.SMOOTH_RED_SANDSTONE);
+      registerBackAndForthAllStates(Blocks.SMOOTH_RED_SANDSTONE, Blocks.SMOOTH_SANDSTONE);
+      registerBackAndForthAllStates(Blocks.SANDSTONE_SLAB, Blocks.RED_SANDSTONE_SLAB);
+      registerBackAndForthAllStates(Blocks.RED_SANDSTONE_SLAB, Blocks.SANDSTONE_SLAB);
+      registerBackAndForthAllStates(Blocks.SANDSTONE_STAIRS, Blocks.RED_SANDSTONE_STAIRS);
+      registerBackAndForthAllStates(Blocks.RED_SANDSTONE_STAIRS, Blocks.SANDSTONE_STAIRS);
+      registerBackAndForthAllStates(Blocks.SANDSTONE_WALL, Blocks.RED_SANDSTONE_WALL);
+      registerBackAndForthAllStates(Blocks.RED_SANDSTONE_WALL, Blocks.SANDSTONE_WALL);
+      registerBackAndForthAllStates(Blocks.SMOOTH_SANDSTONE_STAIRS, Blocks.SMOOTH_RED_SANDSTONE_STAIRS);
+      registerBackAndForthAllStates(Blocks.SMOOTH_RED_SANDSTONE_STAIRS, Blocks.SMOOTH_SANDSTONE_STAIRS);
+      registerBackAndForthAllStates(Blocks.SMOOTH_SANDSTONE_SLAB, Blocks.SMOOTH_RED_SANDSTONE_SLAB);
+      registerBackAndForthAllStates(Blocks.SMOOTH_RED_SANDSTONE_SLAB, Blocks.SMOOTH_SANDSTONE_SLAB);
+      registerBackAndForthAllStates(Blocks.CUT_SANDSTONE, Blocks.CUT_RED_SANDSTONE);
+      registerBackAndForthAllStates(Blocks.CUT_RED_SANDSTONE, Blocks.CUT_SANDSTONE);
+      registerBackAndForthAllStates(Blocks.CUT_SANDSTONE_SLAB, Blocks.CUT_RED_SANDSTONE_SLAB);
+      registerBackAndForthAllStates(Blocks.CUT_RED_SANDSTONE_SLAB, Blocks.CUT_SANDSTONE_SLAB);
+      
+      // Glazed Terracotta
+      registerConsecutivePairsAllStates(Blocks.WHITE_GLAZED_TERRACOTTA, Blocks.ORANGE_GLAZED_TERRACOTTA, Blocks.MAGENTA_GLAZED_TERRACOTTA, Blocks.LIGHT_BLUE_GLAZED_TERRACOTTA, Blocks.YELLOW_GLAZED_TERRACOTTA,
+				Blocks.LIME_GLAZED_TERRACOTTA, Blocks.PINK_GLAZED_TERRACOTTA, Blocks.GRAY_GLAZED_TERRACOTTA, Blocks.LIGHT_GRAY_GLAZED_TERRACOTTA, Blocks.CYAN_GLAZED_TERRACOTTA,
+				Blocks.PURPLE_GLAZED_TERRACOTTA, Blocks.BLUE_GLAZED_TERRACOTTA, Blocks.BROWN_GLAZED_TERRACOTTA, Blocks.GREEN_GLAZED_TERRACOTTA, Blocks.RED_GLAZED_TERRACOTTA, Blocks.BLACK_GLAZED_TERRACOTTA);
+
+      // Nature
+      registerConsecutivePairs(Blocks.ALLIUM, Blocks.AZURE_BLUET,  Blocks.BLUE_ORCHID,  Blocks.CORNFLOWER, Blocks. DANDELION,  Blocks.LILAC, Blocks.LILY_OF_THE_VALLEY, Blocks.ORANGE_TULIP,  Blocks.OXEYE_DAISY, Blocks.PEONY, Blocks.PINK_TULIP,  Blocks.POPPY,  Blocks.RED_TULIP,  Blocks.ROSE_BUSH,  Blocks.SUNFLOWER,  Blocks.WHITE_TULIP);
+      registerBackAndForthAllStates(Blocks.LARGE_FERN, Blocks.FERN);
+      registerBackAndForthAllStates(Blocks.FERN, Blocks.LARGE_FERN);
+      registerBackAndForthAllStates(Blocks.TALL_GRASS, Blocks.GRASS);
+      registerBackAndForthAllStates(Blocks.GRASS, Blocks.TALL_GRASS);
+      registerConsecutivePairsAllStates(Blocks.DIRT, Blocks.COARSE_DIRT, Blocks.ROOTED_DIRT, Blocks.DIRT_PATH);
+      registerConsecutivePairsAllStates(Blocks.GRASS_BLOCK, Blocks.MYCELIUM, Blocks.PODZOL);
+
+      // Dyed Things
+      registerConsecutivePairsAllStates(Blocks.CANDLE, Blocks.RED_CANDLE, Blocks.BLUE_CANDLE, Blocks.PURPLE_CANDLE, Blocks.BLACK_CANDLE, Blocks.BROWN_CANDLE, Blocks.CYAN_CANDLE, Blocks.GREEN_CANDLE, Blocks.GRAY_CANDLE, Blocks.LIGHT_BLUE_CANDLE, Blocks.LIGHT_GRAY_CANDLE, Blocks.LIME_CANDLE, Blocks.MAGENTA_CANDLE, Blocks.ORANGE_CANDLE, Blocks.PINK_CANDLE,Blocks.WHITE_CANDLE, Blocks.YELLOW_CANDLE);
+      registerConsecutivePairsAllStates(Blocks.RED_BED, Blocks.BLUE_BED, Blocks.PURPLE_BED, Blocks.BLACK_BED, Blocks.BROWN_BED, Blocks.CYAN_BED, Blocks.GREEN_BED, Blocks.GRAY_BED, Blocks.LIGHT_BLUE_BED, Blocks.LIGHT_GRAY_BED, Blocks.LIME_BED, Blocks.MAGENTA_BED, Blocks.ORANGE_BED, Blocks.PINK_BED,Blocks.WHITE_BED, Blocks.YELLOW_BED);
+      registerConsecutivePairsAllStates(Blocks.RED_CANDLE_CAKE, Blocks.BLUE_CANDLE_CAKE, Blocks.PURPLE_CANDLE_CAKE, Blocks.BLACK_CANDLE_CAKE, Blocks.BROWN_CANDLE_CAKE, Blocks.CYAN_CANDLE_CAKE, Blocks.GREEN_CANDLE_CAKE, Blocks.GRAY_CANDLE_CAKE, Blocks.LIGHT_BLUE_CANDLE_CAKE, Blocks.LIGHT_GRAY_CANDLE_CAKE, Blocks.LIME_CANDLE_CAKE, Blocks.MAGENTA_CANDLE_CAKE, Blocks.ORANGE_CANDLE_CAKE, Blocks.PINK_CANDLE_CAKE,Blocks.WHITE_CANDLE_CAKE, Blocks.YELLOW_CANDLE_CAKE);
+      registerConsecutivePairsAllStates(Blocks.SHULKER_BOX, Blocks.RED_SHULKER_BOX, Blocks.BLUE_SHULKER_BOX, Blocks.PURPLE_SHULKER_BOX, Blocks.BLACK_SHULKER_BOX, Blocks.BROWN_SHULKER_BOX, Blocks.CYAN_SHULKER_BOX, Blocks.GREEN_SHULKER_BOX, Blocks.GRAY_SHULKER_BOX, Blocks.LIGHT_BLUE_SHULKER_BOX, Blocks.LIGHT_GRAY_SHULKER_BOX, Blocks.LIME_SHULKER_BOX, Blocks.MAGENTA_SHULKER_BOX, Blocks.ORANGE_SHULKER_BOX, Blocks.PINK_SHULKER_BOX,Blocks.WHITE_SHULKER_BOX, Blocks.YELLOW_SHULKER_BOX);
+
+      // Deep Slate
+      
 	}
 
 	@Nullable
